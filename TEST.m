b48bb62da6c42f7f039f51d7b2c47ef9f55ff71c
Arnau <blancoarnau@gmail.com>
2020-11-29 19:03:34 +0100

    some modifications

---
+++ TEST.m
@@ -0,0 +1,60 @@
+clear all;
+close all;
+
+%% Data acquistion
+
+data_dir = 'C:/Users/Arnau/Desktop/TFG/output/';
+%_space-MNI152NLin2009cAsym_res-2_desc-preproc
+%data_dir = 'C:/Users/Arnau/Documents/TFG/ds002715/';
+nRuns = 4;
+subject = 3;
+
+figure;
+for i=1:nRuns
+    
+    run = i;
+    disp(run);
+
+    if subject < 10
+        s = "sub-0" + string(subject);
+    else
+        s = "sub-" + string(subject);
+    end
+    
+    if subject < 2
+        file = s + "_ses-mri_task-AVScenesshort_run-" + string(run) + "_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz";
+    else
+        file = s + "_ses-mri_task-AVSceneslong_run-" + string(run) + "_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz";
+    end
+    
+    path = data_dir + s + "/" + file;
+
+    V = niftiread(path);
+    info = niftiinfo(path);
+
+   %%% Data visualization
+
+    x = 30;
+    y = 30;
+    z = 30;
+    
+    voxel = double(reshape(V(x,y,z,:),1,size(V,4)));
+    plot(1:size(voxel,2),voxel);
+    title('Voxel ('+string(x)+','+string(y)+','+string(z)+')');
+    hold on
+
+    dm = ones(size(voxel,2),2);
+    dm(1:50,2) = 0;
+    [B,DEV,STATS] = glmfit(dm,voxel,'normal','constant','off');
+
+    y = B(1)*dm(:,1)' + B(2)*dm(:,2)';
+    %plot(1:size(voxel,2),y);
+    hold on
+
+end
+
+%% To open .tsv files
+
+fid = fopen(a);
+C = textscan(fid, '%f %f %s', 'HeaderLines', 1);
+fclose(fid);
---
+++ run_all_sub_main_analysis.m
@@ -0,0 +1,18 @@
+% Run the single SVM for all subjects
+
+subjects = 8;
+CondClass = [1 2 3]; % Conditions to classify: Forest, People and Traffic (1, 2 and 3)
+
+
+for subject = 1:subjects
+    for POIfile_ind = 1:3    % 3 POIs: Aud, Motor and EVC
+        if POIfile_ind == 3 % EVC: EVC, V1, V2, V3, V1 fovea, V1 periphery, V1 far periphery, V2 fovea, etc. (total=16)
+            for patch = 1:16
+                run_single_sub_CollapseHem_main_analysis_parallel(subject,patch,CondClass,POIfile_ind);
+            end
+        else
+            patch = 1;
+            run_single_sub_CollapseHem_main_analysis_parallel(subject,patch,CondClass,POIfile_ind);
+        end
+    end
+end
